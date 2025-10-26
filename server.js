48a9b087b3ca283b30b8cb058902b4f328b7bf2e
Amal Aaron <amalaaron07@gmail.com>
2025-10-26 07:53:14 +0530

    update1

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 // Optional: handle form redirect
-app.get("/home.html", (req, res) => {
+app.get("home.html", (req, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
