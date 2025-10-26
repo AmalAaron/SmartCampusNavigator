f87245be336bb43249faaec6075ca05bac6ce7ec
Amal Aaron <amalaaron07@gmail.com>
2025-10-26 08:00:34 +0530

    upadate2

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const PORT = 5000;
 
-// Serve static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
 
-// Serve main page
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/home.html", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
+app.get("/profile.html", (req, res) => res.sendFile(path.join(__dirname, "profile.html")));
 
-// Optional: handle form redirect
-app.get("home.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
