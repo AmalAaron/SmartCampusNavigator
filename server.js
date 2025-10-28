2a789fc98ffff2f85b83e08d3309e6431689f5dc
Amal Aaron <amalaaron07@gmail.com>
2025-10-28 22:59:44 +0530

    up

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/home.html", (req, res) => res.sendFile(path.join(__dirname, "home.html")));
+app.get("/profile.html", (req, res) => res.sendFile(path.join(__dirname, "profile.html")));
+
+app.listen(PORT, () => console.log(Server running at http://localhost:${PORT}));
